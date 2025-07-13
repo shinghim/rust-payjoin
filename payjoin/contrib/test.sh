ab568e32096a4b5ef12bbfef51df8af54c6c9e74
Shing Him Ng <shinghim@protonmail.com>
2025-07-13 15:24:39 -0500

    Move BDK integration tests from payjoin-ffi to rust-payjoin

--- payjoin/contrib/test.sh
+++ payjoin/contrib/test.sh
@@ -3,3 +3,4 @@ set -e
 
 cargo test --locked --package payjoin --verbose --all-features --lib
 cargo test --locked --package payjoin --verbose --all-features --test integration
+cargo test --locked --package payjoin --verbose --all-features --test bdk_integration_test
